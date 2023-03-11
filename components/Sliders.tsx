ebb81cf2cc09924a0601e9061119c8852ce380a3
Favour Akomolafe <akomolafefavour@gmail.comm>
2023-03-11 17:52:13 +0100

    testimonial slider

--- components/Sliders.tsx
+++ components/Sliders.tsx
@@ -1,25 +1,9 @@
 import Image from 'next/image'
 import Slider from 'react-slick'
+import data from '../data/testimonials.json'
 
 export const Testimonials = () => {
-  const testimonials = [
-    {
-      name: 'Amina Mohammed Abdullahi',
-      pic: '',
-      title: 'Field Supervisor',
-      testimonial:
-        'AgLane is the best and most outstanding company I have ever worked with. I am proud to be part of the AgLane family.',
-    },
-    {
-      name: 'Moses Oke',
-      pic: '',
-      title: 'Field Manager',
-      testimonial:
-        'AgLane Nigeria Ltd. has created an enabling environment for me to learn about agricultural innovation. The organization has provided me with a platform for disseminating agricultural information to rural communities.',
-    }
-  ]
-
-  var settings = {
+  const settings = {
     dots: true,
     infinite: true,
     speed: 500,
@@ -29,22 +13,21 @@ export const Testimonials = () => {
 
   return (
     <>
-    <Slider {...settings}>
-      { testimonials && testimonials.map((testimonial, index) => (
-        <div key={index} className="flex flex-row">`
-        {/* <div className=""><Image src={testimonial.pic} width={400} height={400} alt={testimonial.name}/></div> */}
-      <div className="flex flex-col">
-          <p>
-            {testimonial.testimonial}
-          </p>
-          <div className="">
-            <h3>{testimonial.name}</h3>
-            <span>{testimonial.title}</span>
-          </div>
-        </div>
-      </div>
-      ))}
-    </Slider>
+      <Slider {...settings}>
+        {data &&
+          data.data.map((person, index) => (
+            <div key={index} className="flex flex-row">
+              {/* <div className=""><Image src={testimonial.pic} width={400} height={400} alt={testimonial.name}/></div> */}
+              <div className="flex flex-col">
+                <p>{person.testimonial}</p>
+                <div className="">
+                  <h3>{person.name}</h3>
+                  <span>{person.title}</span>
+                </div>
+              </div>
+            </div>
+          ))}
+      </Slider>
     </>
   )
 }
